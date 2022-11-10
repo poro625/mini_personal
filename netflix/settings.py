771ce7535470c529434e4aece4b98c94d745ab75
poro625 <poro625@naver.com>
2022-11-10 20:48:04 +0900

    [생성] simple-jwt url

--- netflix/settings.py
+++ netflix/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    'DEFAULT_PERMISSION_CLASSES': [        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
