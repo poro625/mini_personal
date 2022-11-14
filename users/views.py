151f036a5346d1b071ba9b02ae9ff8cd50dc85c7
poro625 <poro625@naver.com>
2022-11-14 10:45:31 +0900

    [추가] users view 작성

--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import UserViewSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
+class UserView(APIView):
 
-class ProfileView():
-    def :
-        return
+    def post(self, request, format=None):
+        serializer = UserViewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
