ec593b3778aa6020bf6835a3880940807d64bc9d
poro625 <poro625@naver.com>
2022-11-15 14:22:42 +0900

    [수정]

--- users/serializers.py
+++ users/serializers.py
@@ -3,25 +3,30 @@ from users.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
-class UserViewSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
     def create(seif, vaildated_data):
+        user = super().create(vaildated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
+        return
 
-        
+    def update(seif, vaildated_data):
+        user = super().create(vaildated_data)
+        password = user.password
+        user.set_password(password)
+        user.save()
         return
 
 
 
-class CustomTokenObtainPairView(TokenObtainPairSerializer):
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
-        # Add custom claims
         token['email'] = user.email
-        # ...
-
         return token
--- users/views.py
+++ users/views.py
@@ -1,24 +1,22 @@
 from users.models import User
-from users.serializers import UserViewSerializer, CustomTokenObtainPairView
+from users.serializers import UserSerializer, CustomTokenObtainPairSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
 )
 
 class UserView(APIView):
-
     def post(self, request, format=None):
-        serializer = UserViewSerializer(data=request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({"message":"가입완료"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairView
+    serializer_class = CustomTokenObtainPairSerializer
