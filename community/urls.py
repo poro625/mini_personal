ee3bb82207f567981db590d9aac3ec2561fc06ea
poro625 <poro625@naver.com>
2022-11-18 11:26:00 +0900

    [수정] url

--- community/urls.py
+++ community/urls.py
@@ -5,12 +5,9 @@ from community import views
 
 urlpatterns = [
 
-    path('', views.MovieView.as_view(), name='Movie_View'),
+    path('', views.ArticleView.as_view(), name='Article_View'),
     path('<int:community_id>/', views.MovieView.as_view(), name='Movie_View'),
     path('<int:community_id>/Movies/', views.MovieView.as_view(), name='Movie_View'),
     path('', views.MovieView.as_view(), name='Movie_View'),
     
 ]
-    
-    
-
