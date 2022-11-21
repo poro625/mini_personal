0ecc732f08bc09dc67352b50e19630468a5bca9c
poro625 <poro625@naver.com>
2022-11-21 22:27:46 +0900

    [수정] url수정

--- community/urls.py
+++ community/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.CommunityView.as_view(), name='community_view'),
-    path('<int:article_id>/', views.CommunityDetailView.as_view(), name='community_detail_view'),
-    path('<int:article_id>/comment/', views.CommentView.as_view(), name='coment_view'),
-    path('<int:article_id>/comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
-    path('<int:article_id>/like/', views.LikeView.as_view(), name='like_view'),
+    path('<int:community_id>/', views.CommunityDetailView.as_view(), name='community_detail_view'),
+    path('<int:community_id>/comment/', views.CommentView.as_view(), name='coment_view'),
+    path('<int:community_id>/comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
+    path('<int:community_id>/like/', views.LikeView.as_view(), name='like_view'),
 
     
 ]
