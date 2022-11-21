2b912cae5077fc566d7ddca0db0059fa25ebaee3
poro625 <poro625@naver.com>
2022-11-21 14:43:10 +0900

    [추가] pillow 프레임워크 추가

--- community/models.py
+++ community/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from users.models import User
 
 class Movie(models.Model):
-
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
     movie_id = models.CharField(max_length=50)
     image = models.ImageField()
     original_title = models.CharField(max_length=100)
     title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return str(self.title)
